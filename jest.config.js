b5304ab6bed10614e2c3d1136903f81a24b93d59
adam-almeida <adamportifolio@gmail.com>
2022-10-01 00:03:04 -0300

    chore: add push mapping to typescript and jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    roots: ["<rootDir>/src"],
-    collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-    coverageDirectory: "coverage",
-    testEnvironment: "node",
-    transform: {
-        ".+\\.ts$": "ts-jest"
-    },
-    // moduleNameMapper: {
-    //     "@/(.*)": "<rootDir>/src/$1"
-    // }
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
 }
